7b52ce966984694c5b653baeb49cf6823f959706
Hsu You-Ming <telstartx2@gmail.com>
2026-08-28 04:23:33 +0800

    feat: export context pipeline

--- src/context/index.js
+++ src/context/index.js
@@ -1,9 +1,11 @@
 const { loadContext } = require('./loader');
 const { selectContextFiles } = require('./selector');
 const { retrieveDecisions } = require('./decisionRetriever');
+const { buildContext } = require('./pipeline');
 
 module.exports = {
   loadContext,
   selectContextFiles,
-  retrieveDecisions
+  retrieveDecisions,
+  buildContext
 };
