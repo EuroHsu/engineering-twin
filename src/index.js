682a7e71ae8692ad3024f1d603e20cd561ef3b84
Hsu You-Ming <telstartx2@gmail.com>
2026-08-28 10:43:24 +0800

    feat: export Engineering Twin runtime

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { findTwinData } = require("./discovery/twinLocator");
 const { validateTwinData } = require("./validator/twinValidator");
+const { runTwinRuntime } = require("./runtime");
 
 module.exports = {
   findTwinData,
   validateTwinData,
+  runTwinRuntime,
 };
